86c9437e582ad4019a90a99f1a791e63015818c6
sumit singh <sumitsinghitpro@gmail.com>
2018-09-08 17:23:15 +0530

    updated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel By ghosh
                 </div>
 
                 <div class="links">
